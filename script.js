4dfbd5fdc8c3997aaf090646c15cc381a95f3a3f
joaovictorcasado <joaovictorcasado@hotmail.com>
2022-01-28 20:11:13 -0300

    atualizacao da validacao

--- script.js
+++ script.js
@@ -1,57 +1,36 @@
-// Botões de ação ficarão aqui e também a validação..
 
-/* Função Validar */
+// Validação do Formulario
+
+/* Seletores Dom*/
+/*let nome = document.getElementById("username");
+let sobrenome = document.getElementById("surname");
+let email = document.getElementById("email");
+let senha = document.getElementById("password");
+let telefone = document.getElementById("tel");
+let cep = document.getElementById("cep");
+let sexo = document.getElementById("sex");
+let newsletter = document.getElementById("check_box").checked; */
+
+
+
 function validar() {
-  // pegando o valor do nome pelos names
-  let nome = document.getElementById("username");
-  let sobrenome = document.getElementById("surname");
-  let email = document.getElementById("email");
-  let senha = document.getElementById("password");
-  let telefone = document.getElementById("tel");
-  let cep = document.getElementById("cep");
-  let sexo = document.getElementById("sex");
-  let newsletter = document.getElementById("check_box").checked;
 
-  // verificar se o nome está vazio
-  if (nome.value == "") {
-    alert("Nome não informado");
+  // Ao clicar na função irá validar 
+  // os campos vazios diretamente pelo html..
+  // Atraves do atributo required
 
-    // Deixa o input com o focus
-    nome.focus();
-    // retorna a função e não olha as outras linhas
-    return;
-  }
-  if (sobrenome.value == "") {
-    alert("Sobrenome não informado");
-    sobrenome.focus();
-    return;
-  }
-  if (email.value == "") {
-    alert("E-mail não informado");
-    email.focus();
-    return;
-  }
-  if (senha.value == "") {
-    alert("Senha não informada");
-    senha.focus();
-    return;
-  }
-  if (telefone.value == "") {
-    alert("Telefone não informado");
-    telefone.focus();
-    return;
-  }
-  if (cep.value == "") {
-    alert("CEP não informado");
-    cep.focus();
-    return;
-  }
-  if (sexo.value == "") {
-    alert("Informe seu sexo");
-    sexo.focus();
-    return;
-  }
-  alert("Formulário enviado!");
-  // envia o formulário
-  //formulario.submit();
 }
+
+/*
+
+function armazenar (){
+  const login = [nome.value, senha.value]
+  console.log(login)
+}
+
+
+//function armazenar (){
+  
+  //const login = [nome.value, senha.value]
+ // console.log(login)
+//} */
